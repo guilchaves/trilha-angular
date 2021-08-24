fd015431ba37cb50b6e0ddb111d6fed845737aad
Guilherme Chaves <guilgchaves@protonmail.com>
2021-08-24 11:04:47 -0300

    Edit section with ng forms added

--- src/app/courses/course-info/course-info.component.ts
+++ src/app/courses/course-info/course-info.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 
+import { Course } from '../course'
+import { CourseService } from '../course.service'
+
 @Component({
   selector: 'app-course-info',
   templateUrl: './course-info.component.html',
   styleUrls: ['./course-info.component.scss'],
 })
 export class CourseInfoComponent implements OnInit {
-  courseId: number
+  course: Course
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) {}
 
   ngOnInit(): void {
-    this.courseId = +this.activatedRoute.snapshot.paramMap.get('id')
+    this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id'))
+  }
+
+  save(): void {
+    this.courseService.save(this.course)
   }
 }
