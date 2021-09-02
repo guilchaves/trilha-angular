2cd04e6cfecee583cd4629a68a75d28b3033fad7
Guilherme Chaves <guilgchaves@protonmail.com>
2021-09-02 15:40:22 -0300

    ngrx-store added to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeModule } from './pages/home/home.module'
 import { BookmarksModule } from './pages/bookmarks/bookmarks.module';
-import { StoreModule } from '@ngrx/store'
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule, BookmarksModule, StoreModule.forRoot({}, {})],
+  imports: [BrowserModule, AppRoutingModule, HomeModule, BookmarksModule, StoreModule.forRoot({}, {}), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
 })
